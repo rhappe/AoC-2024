8d8784ca536385fecf1fa49710f989604695eaa6
Richard Happe <richhappe@gmail.com>
2024-12-02 09:28:03 -0600

    day 2 part 2

--- src/main/kotlin/puzzles/Day02.kt
+++ src/main/kotlin/puzzles/Day02.kt
@@ -9,6 +9,10 @@ fun main() {
 
     val partOneResult = Day02.Part01.calculateSafeReportCount(reports)
     println("Part 1: $partOneResult")
+
+
+    val partTwoResult = Day02.Part02.calculateSafeReportCount(reports)
+    println("Part 1: $partTwoResult")
 }
 
 private object Day02 {
@@ -27,11 +31,18 @@ private object Day02 {
         }
     }
 
+    object Part02 {
+        fun calculateSafeReportCount(reports: List<RedNosedReport>): Int {
+            val safetyMeasurements = reports.map { it.calculateSafety(applyDampening = true) }
+            return safetyMeasurements.count { it == RedNosedReportSafety.Safe }
+        }
+    }
+
     data class RedNosedReport(
         val values: List<Int>,
     )
 
-    private fun RedNosedReport.calculateSafety(): RedNosedReportSafety {
+    private fun RedNosedReport.calculateSafety(applyDampening: Boolean = false): RedNosedReportSafety {
         val differenceValues = buildList {
             for (i in 0 until (values.size - 1)) {
                 add(values[i + 1] - values[i])
@@ -40,17 +51,24 @@ private object Day02 {
 
         // if all the values are positive or all of the values are negative, then all are increasing or decreasing
         // then, if all differ by 1-3, they are safe. Otherwise, unsafe.
-        return when {
-            (differenceValues.all { it > 0 } || differenceValues.all { it < 0 }) -> {
-                if (differenceValues.all { it.absoluteValue in 1..3 }) {
-                    RedNosedReportSafety.Safe
-                } else {
-                    RedNosedReportSafety.Unsafe
+        if (differenceValues.all { it > 0 } || differenceValues.all { it < 0 }) {
+            if (differenceValues.all { it.absoluteValue in 1..3 }) {
+                return RedNosedReportSafety.Safe
+            }
+        }
+
+        if (applyDampening) {
+            for (index in values.indices) {
+                val dampenedValues = values.toMutableList()
+                dampenedValues.removeAt(index)
+                val dampenedReport = RedNosedReport(dampenedValues)
+                if (dampenedReport.calculateSafety(applyDampening = false) == RedNosedReportSafety.Safe) {
+                    return RedNosedReportSafety.Safe
                 }
             }
-
-            else -> RedNosedReportSafety.Unsafe
         }
+
+        return RedNosedReportSafety.Unsafe
     }
 
     enum class RedNosedReportSafety {
