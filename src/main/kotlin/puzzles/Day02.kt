094aa7db329d127ce7182d0775d53e87562ab999
Richard Happe <richhappe@gmail.com>
2024-12-02 09:35:18 -0600

    some cleanup

--- src/main/kotlin/puzzles/Day02.kt
+++ src/main/kotlin/puzzles/Day02.kt
@@ -5,74 +5,57 @@ import kotlin.math.absoluteValue
 
 fun main() {
     val input = readInput(day = 2)
-    val reports = Day02.parseInput(input)
 
-    val partOneResult = Day02.Part01.calculateSafeReportCount(reports)
+    val partOneResult = Part01.calculateSafeReportCount(input)
     println("Part 1: $partOneResult")
 
-
-    val partTwoResult = Day02.Part02.calculateSafeReportCount(reports)
-    println("Part 1: $partTwoResult")
+    val partTwoResult = Part02.calculateSafeReportCount(input)
+    println("Part 2: $partTwoResult")
 }
 
-private object Day02 {
-    fun parseInput(input: List<String>): List<RedNosedReport> {
-        return input.map { reportData ->
-            RedNosedReport(
-                values = reportData.split(" ").map { it.toInt() },
-            )
+private object Part01 {
+    fun calculateSafeReportCount(input: List<String>): Int {
+        return parseInput(input).count { isSafeReport(it) }
+    }
+}
+
+private object Part02 {
+    fun calculateSafeReportCount(input: List<String>): Int {
+        return parseInput(input).count { isSafeReport(it, removeDampening = true) }
+    }
+}
+
+private fun parseInput(input: List<String>): List<List<Int>> {
+    return input.map { reportData ->
+        reportData.split(" ").map { it.toInt() }
+    }
+}
+
+private fun isSafeReport(values: List<Int>, removeDampening: Boolean = false): Boolean {
+    val differenceValues = buildList {
+        for (i in 0 until (values.size - 1)) {
+            add(values[i + 1] - values[i])
         }
     }
 
-    object Part01 {
-        fun calculateSafeReportCount(reports: List<RedNosedReport>): Int {
-            val safetyMeasurements = reports.map { it.calculateSafety() }
-            return safetyMeasurements.count { it == RedNosedReportSafety.Safe }
+    // if all the values are positive or all of the values are negative, then all are increasing or decreasing
+    // then, if all differ by 1-3, they are safe. Otherwise, unsafe.
+    if (differenceValues.all { it > 0 } || differenceValues.all { it < 0 }) {
+        if (differenceValues.all { it.absoluteValue in 1..3 }) {
+            return true
         }
     }
 
-    object Part02 {
-        fun calculateSafeReportCount(reports: List<RedNosedReport>): Int {
-            val safetyMeasurements = reports.map { it.calculateSafety(applyDampening = true) }
-            return safetyMeasurements.count { it == RedNosedReportSafety.Safe }
-        }
-    }
-
-    data class RedNosedReport(
-        val values: List<Int>,
-    )
-
-    private fun RedNosedReport.calculateSafety(applyDampening: Boolean = false): RedNosedReportSafety {
-        val differenceValues = buildList {
-            for (i in 0 until (values.size - 1)) {
-                add(values[i + 1] - values[i])
+    // if dampening should be removed, we will re-calculate the safety for the report for each value removed.
+    if (removeDampening) {
+        for (index in values.indices) {
+            val dampenedValues = values.toMutableList().apply { removeAt(index) }
+            if (isSafeReport(values = dampenedValues, removeDampening = false)) {
+                return true
             }
         }
-
-        // if all the values are positive or all of the values are negative, then all are increasing or decreasing
-        // then, if all differ by 1-3, they are safe. Otherwise, unsafe.
-        if (differenceValues.all { it > 0 } || differenceValues.all { it < 0 }) {
-            if (differenceValues.all { it.absoluteValue in 1..3 }) {
-                return RedNosedReportSafety.Safe
-            }
-        }
-
-        if (applyDampening) {
-            for (index in values.indices) {
-                val dampenedValues = values.toMutableList()
-                dampenedValues.removeAt(index)
-                val dampenedReport = RedNosedReport(dampenedValues)
-                if (dampenedReport.calculateSafety(applyDampening = false) == RedNosedReportSafety.Safe) {
-                    return RedNosedReportSafety.Safe
-                }
-            }
-        }
-
-        return RedNosedReportSafety.Unsafe
     }
 
-    enum class RedNosedReportSafety {
-        Safe,
-        Unsafe,
-    }
+    // if we got here, the report is unsafe.
+    return false
 }
