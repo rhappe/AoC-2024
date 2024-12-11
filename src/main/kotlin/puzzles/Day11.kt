bbb905359cfe0f4b8fc91a11d349c0bd928d50bf
Richard Happe <richhappe@gmail.com>
2024-12-11 09:25:56 -0600

    day 11 parts 1 and 2 solutions

---
+++ src/main/kotlin/puzzles/Day11.kt
@@ -0,0 +1,78 @@
+package puzzles
+
+import api.readInput
+import kotlin.time.DurationUnit
+import kotlin.time.measureTimedValue
+
+fun main() {
+    val input = readInput(day = 11).single()
+
+    val partOneAnswer = measureTimedValue { Day11.Part01.getResultingStonesCount(input) }
+    println("Part 1: ${partOneAnswer.value}; Duration: ${partOneAnswer.duration.toString(DurationUnit.SECONDS, 4)}")
+
+    val partTwoAnswer = measureTimedValue { Day11.Part02.getResultingStonesCount(input) }
+    println("Part 2: ${partTwoAnswer.value}; Duration: ${partTwoAnswer.duration.toString(DurationUnit.SECONDS, 4)}")
+}
+
+private object Day11 {
+    object Part01 {
+        fun getResultingStonesCount(input: String): Long {
+            val pluto = Pluto(parseStoneValues(input))
+            return pluto.countStonesAfterBlinks(times = 25)
+        }
+    }
+
+    object Part02 {
+        fun getResultingStonesCount(input: String): Long {
+            val pluto = Pluto(parseStoneValues(input))
+            return pluto.countStonesAfterBlinks(times = 75)
+        }
+    }
+
+    private fun parseStoneValues(input: String): List<Long> {
+        return input.split(" ").map { it.toLong() }
+    }
+
+    private class Pluto(private val initialStones: List<Long>) {
+        // keep track of the stone value paired with the number of starting blinks
+        // mapped to the number of stones that resulted after said blinks have occurred.
+        private val trackedStones = mutableMapOf<Pair<Long, Int>, Long>()
+
+        fun countStonesAfterBlinks(times: Int): Long {
+            return initialStones.sumOf { countStonesAfterBlinks(it, times) }
+        }
+
+        private fun countStonesAfterBlinks(stoneValue: Long, remaining: Int): Long {
+            val trackedValue = trackedStones[stoneValue to remaining]
+            if (trackedValue != null) {
+                return trackedValue
+            }
+
+            val stonesCount = when {
+                remaining == 0 -> 1L
+                stoneValue == 0L -> countStonesAfterBlinks(
+                    stoneValue = 1L,
+                    remaining = remaining - 1,
+                )
+
+                stoneValue.toString().length % 2 == 0 -> {
+                    val splitStones = listOf(
+                        stoneValue.toString().substring(0, stoneValue.toString().length / 2).toLong(),
+                        stoneValue.toString().substring(stoneValue.toString().length / 2).toLong(),
+                    )
+                    splitStones.sumOf {
+                        countStonesAfterBlinks(stoneValue = it, remaining = remaining - 1)
+                    }
+                }
+
+                else -> countStonesAfterBlinks(
+                    stoneValue = stoneValue * 2024,
+                    remaining = remaining - 1,
+                )
+            }
+
+            trackedStones[stoneValue to remaining] = stonesCount
+            return stonesCount
+        }
+    }
+}
