5428efb01547f54169d45772319c887e3b1c35ad
Richard Happe <richhappe@gmail.com>
2024-12-18 09:14:36 -0600

    day 17 part 1

---
+++ src/main/kotlin/puzzles/Day17.kt
@@ -0,0 +1,230 @@
+package puzzles
+
+import api.readInput
+import utils.printAnswer
+import java.io.OutputStream
+import kotlin.math.pow
+import kotlin.time.measureTimedValue
+
+fun main() {
+    val input = readInput(day = 17)
+
+    val partOneAnswer = measureTimedValue { Day17.Part01.foo(input) }
+    partOneAnswer.printAnswer(label = "Part 1")
+
+    val partTwoAnswer = measureTimedValue { Day17.Part02.foo(input) }
+    partTwoAnswer.printAnswer(label = "Part 2")
+}
+
+private object Day17 {
+    object Part01 {
+        fun foo(input: List<String>): String {
+            val completed = parseInput(input).runProgram()
+            return completed.outputBuffer.toString()
+        }
+    }
+
+    object Part02 {
+        fun foo(input: List<String>): String {
+            return parseInput(input).outputBuffer.toString()
+        }
+    }
+
+    private fun parseInput(input: List<String>): Computer {
+        val registerAString = input[0]
+        val registerBString = input[1]
+        val registerCString = input[2]
+        val programString = input[4]
+
+        return Computer(
+            registerA = registerAString.split(": ")[1].toInt(),
+            registerB = registerBString.split(": ")[1].toInt(),
+            registerC = registerCString.split(": ")[1].toInt(),
+            program = programString.split(": ")[1].split(",").map(String::toInt),
+        )
+    }
+
+    private class StandardOutput : OutputStream() {
+        private val buffer = mutableListOf<Int>()
+
+        override fun write(b: Int) {
+            buffer += b
+        }
+
+        override fun toString(): String {
+            return buffer.joinToString(",")
+        }
+    }
+
+    private data class Computer(
+        val registerA: Int,
+        val registerB: Int,
+        val registerC: Int,
+        val program: List<Int>,
+        val pointer: Int = 0,
+        val outputBuffer: OutputStream = StandardOutput(),
+    ) {
+        private val currentInstruction: Instruction? = when (pointer) {
+            in program.indices -> Instruction(
+                operator = Operator(program[pointer]),
+                operand = Operand(program[pointer + 1]),
+            )
+
+            else -> null
+        }
+
+        fun runProgram(): Computer {
+            return currentInstruction?.invoke(this)?.runProgram() ?: this
+        }
+
+        fun print(value: Int) = outputBuffer.write(value)
+    }
+
+    private data class Instruction(
+        val operator: Operator,
+        val operand: Operand,
+    ) {
+        operator fun invoke(computer: Computer): Computer {
+            return operator.invoke(computer, operand)
+        }
+    }
+
+    private fun Operator(value: Int): Operator = when (value) {
+        0 -> Operator.Adv
+        1 -> Operator.Bxl
+        2 -> Operator.Bst
+        3 -> Operator.Jnz
+        4 -> Operator.Bxc
+        5 -> Operator.Out
+        6 -> Operator.Bdv
+        7 -> Operator.Cdv
+        else -> error("Invalid operator: $value")
+    }
+
+    private fun Operand(value: Int): Operand = when (value) {
+        in 0..3 -> Operand.Literal(value)
+        4 -> Operand.RegisterA(value)
+        5 -> Operand.RegisterB(value)
+        6 -> Operand.RegisterC(value)
+        else -> error("Invalid operand: $value")
+    }
+
+    private sealed interface Operator {
+        fun invoke(computer: Computer, operand: Operand): Computer
+
+        // opcode 0
+        data object Adv : Operator {
+            override fun invoke(computer: Computer, operand: Operand): Computer {
+                val numerator = computer.registerA
+                val denominator = 2.0.pow(operand.comboValue(computer))
+                return computer.copy(
+                    registerA = (numerator / denominator).toInt(),
+                    pointer = computer.pointer + 2,
+                )
+            }
+        }
+
+        // opcode 1
+        data object Bxl : Operator {
+            override fun invoke(computer: Computer, operand: Operand): Computer {
+                return computer.copy(
+                    registerB = computer.registerB xor operand.literal,
+                    pointer = computer.pointer + 2,
+                )
+            }
+        }
+
+        // opcode 2
+        data object Bst : Operator {
+            override fun invoke(computer: Computer, operand: Operand): Computer {
+                return computer.copy(
+                    registerB = operand.comboValue(computer) % 8,
+                    pointer = computer.pointer + 2,
+                )
+            }
+        }
+
+        // opcode 3
+        data object Jnz : Operator {
+            override fun invoke(computer: Computer, operand: Operand): Computer {
+                return when {
+                    computer.registerA == 0 -> computer.copy(pointer = computer.pointer + 2)
+                    else -> computer.copy(pointer = operand.literal)
+                }
+            }
+        }
+
+        // opcode 4
+        data object Bxc : Operator {
+            override fun invoke(computer: Computer, operand: Operand): Computer {
+                return computer.copy(
+                    registerB = computer.registerB xor computer.registerC,
+                    pointer = computer.pointer + 2,
+                )
+            }
+        }
+
+        // opcode 5
+        data object Out : Operator {
+            override fun invoke(computer: Computer, operand: Operand): Computer {
+                computer.print(operand.comboValue(computer) % 8)
+                return computer.copy(pointer = computer.pointer + 2)
+            }
+        }
+
+        // opcode 6
+        data object Bdv : Operator {
+            override fun invoke(computer: Computer, operand: Operand): Computer {
+                val numerator = computer.registerA
+                val denominator = 2.0.pow(operand.comboValue(computer))
+                return computer.copy(
+                    registerB = (numerator / denominator).toInt(),
+                    pointer = computer.pointer + 2,
+                )
+            }
+
+        }
+
+        // opcode 7
+        data object Cdv : Operator {
+            override fun invoke(computer: Computer, operand: Operand): Computer {
+                val numerator = computer.registerA
+                val denominator = 2.0.pow(operand.comboValue(computer))
+                return computer.copy(
+                    registerC = (numerator / denominator).toInt(),
+                    pointer = computer.pointer + 2,
+                )
+            }
+        }
+    }
+
+    private sealed interface Operand {
+        val literal: Int
+
+        fun comboValue(computer: Computer): Int
+
+        data class Literal(override val literal: Int) : Operand {
+            override fun comboValue(computer: Computer): Int {
+                return literal
+            }
+        }
+
+        data class RegisterA(override val literal: Int) : Operand {
+            override fun comboValue(computer: Computer): Int {
+                return computer.registerA
+            }
+        }
+
+        data class RegisterB(override val literal: Int) : Operand {
+            override fun comboValue(computer: Computer): Int {
+                return computer.registerB
+            }
+        }
+
+        data class RegisterC(override val literal: Int) : Operand {
+            override fun comboValue(computer: Computer): Int {
+                return computer.registerC
+            }
+        }
+    }
+}
