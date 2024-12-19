3aa60424416733badffcafddb7e982588dd79024
Richard Happe <richhappe@gmail.com>
2024-12-19 03:35:50 -0600

    getting close on day 16 pt 2

--- src/main/kotlin/puzzles/Day16.kt
+++ src/main/kotlin/puzzles/Day16.kt
@@ -1,15 +1,20 @@
 package puzzles
 
 import api.readInput
+import input.FakeInput
+import model.Coordinate
+import model.Direction
+import model.IntCoordinate
+import utils.DijkstraResult
+import utils.dijkstra
 import utils.printAnswer
-import kotlin.time.DurationUnit
 import kotlin.time.measureTimedValue
 
 fun main() {
-    val input = readInput(day = 16)
+    val input = FakeInput.Day16.input//readInput(day = 16)
 
     val partOneAnswer = measureTimedValue { Day16.Part01.foo(input) }
-    partOneAnswer.printAnswer(label = "Part 2")
+    partOneAnswer.printAnswer(label = "Part 1")
 
     val partTwoAnswer = measureTimedValue { Day16.Part02.foo(input) }
     partTwoAnswer.printAnswer(label = "Part 2")
@@ -18,13 +23,91 @@ fun main() {
 private object Day16 {
     object Part01 {
         fun foo(input: List<String>): Int {
-            TODO()
+            val maze = parseMaze(input)
+            val endCandidates = maze.traverse().distances.filter { it.key.first == maze.end }
+            return endCandidates.minOf { it.value }
         }
     }
 
     object Part02 {
         fun foo(input: List<String>): Int {
-            TODO()
+            val maze = parseMaze(input)
+            val path = maze.traverse().getAllShortestPathsTo(maze.end to Direction.North) { item ->
+                Direction.entries.map { (item.first + it) to it }
+                    .flatMap { adjacent ->
+                        Direction.entries.filter { it != adjacent.second }.map { direction ->
+                            adjacent.first to direction
+                        }
+                    }.toSet()
+            }
+            return path.flatten().distinctBy { it.first }.count()
         }
+    }
+
+    private data class Maze(
+        val paths: Set<IntCoordinate>,
+        val walls: Set<IntCoordinate>,
+        val start: IntCoordinate,
+        val end: IntCoordinate,
+    ) {
+        val height = walls.maxOf { it.row }
+        val width = walls.maxOf { it.col }
+
+        private val directionalPaths = paths.flatMap { path ->
+            Direction.entries.map { path to it }
+        }
+
+        fun traverse(): DijkstraResult<Pair<IntCoordinate, Direction>> {
+            return dijkstra(directionalPaths, start to Direction.East) { (coordinate, direction) ->
+                Direction.entries.map { (coordinate + it) to it }
+                    .associateWith { if (it.second != direction) 1001 else 1 }
+                    .toList()
+                    .toSet()
+            }
+        }
+
+        override fun toString(): String = buildString {
+            for (row in 0..height) {
+                for (col in 0..width) {
+                    when (Coordinate(row, col)) {
+                        in walls -> append("#")
+                        in paths -> append(".")
+                        start -> append("S")
+                        end -> append("E")
+                    }
+                }
+                appendLine()
+            }
+        }
+    }
+
+    private fun parseMaze(input: List<String>): Maze {
+        val paths = mutableSetOf<IntCoordinate>()
+        val walls = mutableSetOf<IntCoordinate>()
+        var start: IntCoordinate? = null
+        var end: IntCoordinate? = null
+
+        for (row in input.indices) {
+            for (col in input[row].indices) {
+                val coordinate = Coordinate(row, col)
+                when (input[row][col]) {
+                    '#' -> walls.add(coordinate)
+                    '.' -> paths.add(coordinate)
+                    'S' -> start = coordinate
+                    'E' -> end = coordinate
+                }
+            }
+        }
+
+        if (start == null || end == null) {
+            error("Maze is missing either start or end.")
+        }
+
+        return Maze(
+            paths = paths + start + end,
+            walls = walls,
+            start = start,
+            end = end,
+        )
     }
 }
--- src/main/kotlin/utils/Dijkstra.kt
+++ src/main/kotlin/utils/Dijkstra.kt
@@ -8,11 +8,16 @@ data class DijkstraResult<T>(
     val neighborsBlock: (T) -> Set<T>,
 ) {
     infix fun shortestPathTo(end: T): ArrayDeque<T>? {
+        return getShortestPathTo(end, neighborsBlock)
+    }
+
+    fun getShortestPathTo(end: T, neighborsBlock: (T) -> Set<T>): ArrayDeque<T>? {
         var currentNode: T = end
         val pathNodes = mutableListOf<T>()
 
         while (currentNode != start) {
-            val next = getValidNeighbors(currentNode).minByOrNull { distances.getValue(it) }
+            val unfilteredNeighbors = neighborsBlock(currentNode).filter { it in distances.keys }.toSet()
+            val next = unfilteredNeighbors.minByOrNull { distances.getValue(it) }
             if (next == null) return null
             pathNodes += currentNode
             currentNode = next
@@ -21,9 +26,39 @@ data class DijkstraResult<T>(
         return ArrayDeque(pathNodes.reversed())
     }
 
-    private fun getValidNeighbors(node: T): Set<T> {
-        return neighborsBlock(node).filter { it in distances.keys }.toSet()
+    fun getAllShortestPathsTo(end: T, neighborsBlock: (T) -> Set<T>): List<ArrayDeque<T>> {
+        val paths = mutableListOf<List<T>>()
+        val priorityQueue = ArrayDeque<Pair<T, List<T>>>()
+        priorityQueue.add(end to emptyList())
+
+        while (priorityQueue.isNotEmpty()) {
+            val (node, list) = priorityQueue.removeFirst()
+            var currentNode = node
+            val newList = list.toMutableList()
+            while (currentNode != start) {
+                val neighbors = neighborsBlock(currentNode).filter { it in distances.keys }.toSet()
+                // can't do this. The value of the previous node is different depending on whether
+                // a turn happened... But the previous node value + the weight of the jump should equal
+                // the current node value.
+                val minValue = neighbors.minOf { distances.getValue(it) }
+                val allMins = neighbors.filter { distances.getValue(it) == minValue }
+
+                // grab the "next" node and use it in this current iteration
+                val next = allMins.first()
+                // post all the remaining ones to the queue
+                allMins.subList(1, allMins.size).forEach { remaining ->
+                    priorityQueue.add(remaining to newList.toList())
+                }
+
+                newList += next
+                currentNode = next
+            }
+            paths += newList
+        }
+
+        return paths.map { ArrayDeque(it) }
     }
+
 }
 
 typealias WeightedNode<T> = Pair<T, Int>
