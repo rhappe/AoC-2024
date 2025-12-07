5cb8ff97e385e529b1b9038f2d98612fa1febef6
Richard Happe <richhappe@gmail.com>
2025-12-07 12:06:20 -0600

    day 10 parts 1 and 2 (un-optimized)

---
+++ src/main/kotlin/aoc/twenty15/Day10.kt
@@ -0,0 +1,38 @@
+package aoc.twenty15
+
+import aoc.api.readInput
+import solution.Solution
+import utils.printAnswer
+import kotlin.time.measureTimedValue
+
+
+fun main() {
+    val input = readInput(day = 10, year = 2015).first()
+
+    val partOneAnswer = measureTimedValue { Day10(input).part1.solve() }
+    partOneAnswer.printAnswer(label = "Part 1")
+
+    val partTwoAnswer = measureTimedValue { Day10(input).part2.solve() }
+    partTwoAnswer.printAnswer(label = "Part 2")
+}
+
+class Day10(input: String) {
+    private val digitCountRegex = "(.)\\1*".toRegex()
+
+    val part1 = Solution { solve(input, depth = 40) }
+    val part2 = Solution { solve(input, depth = 50) }
+
+    private fun solve(input: String, depth: Int) {
+        var result = input
+        repeat(depth) {
+            val matches = digitCountRegex.findAll(result)
+            val groups = matches.map { it.groups }.toList()
+            result = groups.fold("") { acc, group ->
+                val count = requireNotNull(group[0]).value.length
+                val character = requireNotNull(group[1]).value
+                acc + "$count$character"
+            }
+        }
+        result.length
+    }
+}
---
+++ src/test/kotlin/aoc/twenty15/Day10Test.kt
@@ -0,0 +1,46 @@
+package aoc.twenty15
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day10Test {
+    @Test
+    fun `test part 1`() {
+        assertEquals(
+            expected = "11",
+            actual = Day10("1").part1.solve(),
+        )
+    }
+
+    @Test
+    fun `test part 11`() {
+        assertEquals(
+            expected = "21",
+            actual = Day10("11").part1.solve(),
+        )
+    }
+
+    @Test
+    fun `test part 21`() {
+        assertEquals(
+            expected = "1211",
+            actual = Day10("21").part1.solve(),
+        )
+    }
+
+    @Test
+    fun `test part 1211`() {
+        assertEquals(
+            expected = "111221",
+            actual = Day10("1211").part1.solve(),
+        )
+    }
+
+    @Test
+    fun `test part 111221`() {
+        assertEquals(
+            expected = "312211",
+            actual = Day10("111221").part1.solve(),
+        )
+    }
+}
